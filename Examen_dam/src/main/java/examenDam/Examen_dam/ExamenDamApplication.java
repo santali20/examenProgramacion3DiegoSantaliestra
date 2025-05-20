45d66806e7afe8efe0f596a2bc126c0b1c13d7fb
santali20 <santaliestra20@hotmail.com>
2025-05-20 10:10:25 +0200

    exam

---
+++ Examen_dam/src/main/java/examenDam/Examen_dam/ExamenDamApplication.java
@@ -0,0 +1,13 @@
+package examenDam.Examen_dam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenDamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenDamApplication.class, args);
+	}
+
+}
